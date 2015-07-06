f9dbb9a87109e0bc37ee8c0f3336d169a3f76157
Michael Chan <mijoch@gmail.com>
2015-07-06 09:47:55 +0200

    feat: add lastChild

--- src/__tests__/psuedo-class-test.js
+++ src/__tests__/psuedo-class-test.js
@@ -1,19 +1,37 @@
+/* eslint-env node, jasmine */
 jest.dontMock("../pseudo-class");
 
 var ps = require("../pseudo-class");
 
 describe("pseudo-class", function () {
   describe("firstChild", function () {
-    it("returns true if the first argument is 0", function () {
-      expect(ps.firstChild(0)).toBe(true);
+    it("is truthy if the first argument is 0", function () {
+      expect(ps.firstChild(0)).toBeTruthy();
     });
 
-    it("returns false for any other value", function () {
-      expect(ps.firstChild(null)).toBe(false);
-      expect(ps.firstChild(undefined)).toBe(false);
-      expect(ps.firstChild(false)).toBe(false);
-      expect(ps.firstChild(1)).toBe(false);
-      expect(ps.firstChild(-1)).toBe(false);
+    it("is falsy for any other value", function () {
+      expect(ps.firstChild(null)).toBeFalsy();
+      expect(ps.firstChild(undefined)).toBeFalsy();
+      expect(ps.firstChild(false)).toBeFalsy();
+      expect(ps.firstChild(1)).toBeFalsy();
+      expect(ps.firstChild(-1)).toBeFalsy();
+    });
+  });
+
+  describe("lastChild", function () {
+    it("is truthy when first and second arguments are equal", function () {
+      expect(ps.lastChild(0, [1])).toBeTruthy();
+      expect(ps.lastChild(1, [1, 2])).toBeTruthy();
+      expect(ps.lastChild(2, [1, 2, 3])).toBeTruthy();
+    });
+
+    it("is truthy if the first argument is equal to the second arguments length - 1", function () {
+      expect(ps.lastChild(0, [1, 2, 3])).toBeFalsy();
+      expect(ps.lastChild(1, [1, 2, 3])).toBeFalsy();
+      expect(ps.lastChild(-1, [1, 2, 3])).toBeFalsy();
+      expect(ps.lastChild("ham", [1, 2, 3])).toBeFalsy();
+
+      expect(ps.lastChild(0, [])).toBeFalsy();
     });
   });
 });
--- src/pseudo-class.js
+++ src/pseudo-class.js
@@ -1,5 +1,9 @@
 module.exports = {
   firstChild: function (i) {
-    return (i === 0) ? true : false;
+    return (i === 0);
+  },
+
+  lastChild: function (i, arr) {
+    return (i === arr.length - 1);
   }
 };
